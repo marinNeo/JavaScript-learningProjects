ab850982f53fe4f840d1a374f65b0ac5fb8242c9
marinNeo <78420965+marinNeo@users.noreply.github.com>
2021-02-02 13:43:50 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,95 +1,151 @@
-//game values
-let min = 1,
-    max = 10,
-    winningNumber  = getRandomNum(min, max),
-    guessesLeft = 3;
+// UI variables
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
 
-//UI elements
-const game = document.querySelector('#game'),
-      minNum = document.querySelector('.min-num'),
-      maxNum = document.querySelector('.max-num'),
-      guessBtn = document.querySelector('#guess-btn'),
-      guessInput = document.querySelector('#guess-input'),
-      message = document.querySelector('.message');
 
-//assign UI min and max
-minNum.textContent = min;
-maxNum.textContent = max;
+//event listeners
+loadEventListeners();
 
-//play again event listener
-game.addEventListener('mousedown', function(e) {
-    if(e.target.className === 'play-again') {
-        window.location.reload();
+
+function loadEventListeners() {
+  
+  document.addEventListener('DOMContentLoaded', getTasks);
+  
+  form.addEventListener('submit', addTask);
+  
+  taskList.addEventListener('click', removeTask);
+ 
+  clearBtn.addEventListener('click', clearTasks);
+  
+  filter.addEventListener('keyup', filterTasks);
+}
+
+//tasks
+function getTasks() {
+  let tasks;
+  if(localStorage.getItem('tasks') === null){
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.forEach(function(task){
+   
+    const li = document.createElement('li');
+    
+    li.className = 'collection-item';
+    
+    li.appendChild(document.createTextNode(task));
+    
+    const link = document.createElement('a');
+    
+    link.className = 'delete-item secondary-content';
+    
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    
+    li.appendChild(link);
+
+    
+    taskList.appendChild(li);
+  });
+}
+
+//adding a task
+function addTask(e) {
+  if(taskInput.value === '') {
+    alert('Add a task');
+  }
+
+
+  const li = document.createElement('li');
+  
+  li.className = 'collection-item';
+  li.appendChild(document.createTextNode(taskInput.value));
+  const link = document.createElement('a');
+  link.className = 'delete-item secondary-content';
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  li.appendChild(link);
+
+  taskList.appendChild(li);
+
+  storeTaskInLocalStorage(taskInput.value);
+
+  taskInput.value = '';
+
+  e.preventDefault();
+}
+
+function storeTaskInLocalStorage(task) {
+  let tasks;
+  if(localStorage.getItem('tasks') === null){
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.push(task);
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+//removing a task
+function removeTask(e) {
+  if(e.target.parentElement.classList.contains('delete-item')) {
+    if(confirm('Are You Sure?')) {
+      e.target.parentElement.parentElement.remove();
+
+     
+      removeTaskFromLocalStorage(e.target.parentElement.parentElement);
     }
-})
+  }
+}
 
-//listen for guess
-guessBtn.addEventListener('click', function() {
-    let guess = parseInt(guessInput.value)
 
-    //validate input
-    if(isNaN(guess) || guess < min || guess > max) {
-        setMessage(`Please enter a number between ${min} and ${max}`, 'red') 
+function removeTaskFromLocalStorage(taskItem) {
+  let tasks;
+  if(localStorage.getItem('tasks') === null){
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.forEach(function(task, index){
+    if(taskItem.textContent === task){
+      tasks.splice(index, 1);
     }
+  });
 
-    //check if won
-    if(guess === winningNumber) {
-        
-        gameOver(true, `${winningNumber} is correct. You win.`);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
 
+
+function clearTasks() {
+ 
+  while(taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+
+  
+  clearTasksFromLocalStorage();
+}
+
+
+function clearTasksFromLocalStorage() {
+  localStorage.clear();
+}
+
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1){
+      task.style.display = 'block';
     } else {
-        //wrong number
-        guessesLeft -= 1;
-
-        if(guessesLeft === 0) {
-            //game over lost
-
-            gameOver(false, `Game over. You lose. The correct number was ${winningNumber}`)
-
-        } else {
-            //game continues answer wrong
-
-            //change border color
-        guessInput.style.borderColor = 'red';
-
-        //clear the input
-            guessInput.value = '';
-
-            //tell user its the wrong number and inform of remaining guesses
-            setMessage(`${guess} is not correct. ${guessesLeft} guesses left.`, 'red')
-        }
+      task.style.display = 'none';
     }
-})
-
-//game over
-function gameOver(won, msg) {
-    let color;
-    won === true ? color = 'green' : color = 'red';
-
-    //disable input
-    guessInput.disabled = true;
-    //change border color
-    guessInput.style.borderColor = color;
-
-    //set text color
-    message.style.color = color;
-
-    //set winning message
-    setMessage(msg)
-
-    //play again?
-    guessBtn.value = 'Play again';
-    guessBtn.className += 'play-again';
+  });
 }
-
-//getWinningNum
-function getRandomNum(min, max) {
-    return Math.floor(Math.random()*(max-min+1) + min);
-}
-
-//set Message
-function setMessage(msg, color) {
-    message.style.color = color;
-    message.textContent = msg;
-}
-
